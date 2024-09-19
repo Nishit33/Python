65eb0b82c9bf0f9901c63ea6d4a0abe50ebda878
Nishit33 <nishitgajjar800@gmail.com>
2024-09-19 20:15:23 +0530

    Updated

--- Class Work/September/13-09-2024/task8.py
+++ Class Work/September/13-09-2024/task8.py
@@ -1,3 +1,14 @@
 A = '''"Python"'''
 
 print(A.upper())
+
+
+s = "Python devloper is belongs"
+
+print(len(s))
+
+print(s[0:24:2])
+print(s[3:23:2])
+print(s[:19:3])
+print(s[12:23:2])
+print(s[6:13:5])
