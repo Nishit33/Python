5105ebd0694c0670ac9ea55f4b89d8ef52acfa37
Nishit33 <nishitgajjar800@gmail.com>
2024-09-19 23:05:09 +0530

    Updated

--- Class Work/September/13-09-2024/task8.py
+++ Class Work/September/13-09-2024/task8.py
@@ -12,3 +12,26 @@ print(s[3:23:2])
 print(s[:19:3])
 print(s[12:23:2])
 print(s[6:13:5])
+
+l = [3,4,6,8,4,7,43,1,3,4]
+
+print(l[::-1])         
+print(l[4:5:2])         
+print(l[1:8:3])         #441
+print(l[5:9:5])         # 7
+print(l[-2:-8:-1])        #3,1,43,7,4,8 
+
+
+l=("Python is good language")
+
+print(l[0:6:1])   # python
+
+print(l[7:9:1])     # is
+
+print(l[10:15:2])   # go
+
+print(l[5:20:3]  )  #nso n
+
+print(l[3:21:4])    
+
+
